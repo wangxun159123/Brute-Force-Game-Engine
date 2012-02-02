ab37f899b7738e143983298d13f57a6f9108c538
Sascha Wittkowski <wittus@gmx.de>
2012-02-02 21:23:16 +0100

    Model: added new Adapter serializer and test code

--- code/tests/SerializationTest/main.cpp
+++ code/tests/SerializationTest/main.cpp
@@ -6,10 +6,17 @@
 
 #include <Base/CLogger.h>
 #include <Core/Path.h>
+#include <Core/qv4.h>
 #include <Model/Loader/XmlObjectSerializer.h>
 #include <Model/Loader/XmlSectorSerializer.h>
+#include <Model/Loader/XmlAdapterSerializer.h>
 
 namespace Test {
+
+namespace Elements
+{
+	const std::string root = "root";
+}
 
 void readSector(const std::string& fullFilePath)
 {
@@ -46,6 +53,65 @@ void writeSector(const std::string& fullFilePath)
 	// Write test data as XML
 	boost::shared_ptr<BFG::Loader::SectorSerializer> xmlSs(new BFG::Loader::XmlSectorSerializer(&document));
 	xmlSs->write(sp);
+
+	document.SaveFile();
+}
+
+void readAdapterConfig(const std::string& fullFilePath)
+{
+	// Open the test file
+	TiXmlDocument document(fullFilePath);
+	document.LoadFile();
+
+	TiXmlElement* root = document.FirstChildElement("root");
+	TiXmlElement* adapterConfig = root->FirstChildElement("AdapterConfig");
+
+	BFG::Loader::XmlAdapterConfigSerializer xacs(adapterConfig);
+	BFG::Loader::AdapterConfigT ac;
+	xacs.read(ac);
+	
+	BFG::Loader::AdapterConfigT::second_type::const_iterator it = ac.second.begin();
+
+	infolog << ac.first;	
+	infolog << "==============";	
+	for (; it != ac.second.end(); ++it)
+	{
+		infolog << "Identifier: " << it->mIdentifier;
+		infolog << "Parent Position: " << it->mParentPosition;
+		infolog << "Parent Orientation: " << it->mParentOrientation;
+	}
+}
+
+void writeAdapterConfig(const std::string& fullFilePath)
+{
+	// Open the test file
+	TiXmlDocument document(fullFilePath);
+	document.LoadFile();
+
+	// Make a <root> node
+	TiXmlElement* root = new TiXmlElement(Elements::root);
+	document.LinkEndChild(root);
+
+	// Prepare Test Data
+	BFG::Loader::AdapterConfigT ac;
+	ac.first = "TestAdapters";
+
+	BFG::Adapter a;
+	BFG::Adapter b;
+
+	a.mIdentifier = 123;
+	a.mParentPosition = v3::NEGATIVE_UNIT_Z;
+	a.mParentOrientation = BFG::qv4(1.0, 0.1, 0.2, 0.3);
+
+	b.mIdentifier = 77777;
+	b.mParentPosition = v3(1.0f,0.9f,0.8f);
+	b.mParentOrientation = BFG::qv4(0.0, 1.0, 0.5, 0.1);
+	
+	ac.second.push_back(a);
+	ac.second.push_back(b);
+	
+	BFG::Loader::XmlAdapterConfigSerializer xacs(root);
+	xacs.write(ac);
 	
 	document.SaveFile();
 }
@@ -62,7 +128,10 @@ int main() try
 	infolog << "Full File Path: " << fullFilePath;
 	
 	Test::writeSector(fullFilePath);
-	Test::readSector(fullFilePath);
+	Test::readSector(fullFilePath);	
+	
+	Test::writeAdapterConfig(fullFilePath);
+	Test::readAdapterConfig(fullFilePath);
 }
 catch (std::exception& ex)
 {
