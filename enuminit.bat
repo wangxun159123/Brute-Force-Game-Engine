979995070afb4381063af324f74bd958fc459ea1
Sascha Wittkowski <wittus@gmx.de>
2012-02-25 00:48:16 +0100

    Addendum to AudioBlaster rename

--- enuminit.bat
+++ enuminit.bat
@@ -1,12 +1,12 @@
 @ECHO OFF
-type NUL > ./code/engine/AudioBlaster/Enums.hh
+type NUL > ./code/engine/Audio/Enums.hh
 type NUL > ./code/engine/Controller/Enums.hh
 type NUL > ./code/engine/Core/Enums.hh
 type NUL > ./code/engine/Model/Enums.hh
 type NUL > ./code/engine/Physics/Enums.hh
 type NUL > ./code/engine/View/Enums.hh
 
-type NUL > ./code/engine/AudioBlaster/Enums.cpp
+type NUL > ./code/engine/Audio/Enums.cpp
 type NUL > ./code/engine/Controller/Enums.cpp
 type NUL > ./code/engine/Core/Enums.cpp
 type NUL > ./code/engine/Model/Enums.cpp
