77d4ec6b58660e6bb0547f483ea4288ad515cf00
Sascha Wittkowski <wittus@gmx.de>
2012-09-17 17:56:20 +0200

    added new EventSystem test

---
+++ code/tests/EventTest/MultiThreadedTest.cpp
@@ -0,0 +1,47 @@
+/*    ___  _________     ____          __         
+     / _ )/ __/ ___/____/ __/___ ___ _/_/___ ___ 
+    / _  / _// (_ //___/ _/ / _ | _ `/ // _ | -_)
+   /____/_/  \___/    /___//_//_|_, /_//_//_|__/ 
+                               /___/             
+
+This file is part of the Brute-Force Game Engine, BFG-Engine
+
+For the latest info, see http://www.brute-force-games.com
+
+Copyright (c) 2012 Brute-Force Games GbR
+
+The BFG-Engine is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+The BFG-Engine is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with the BFG-Engine. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <EventSystem/Emitter.h>
+
+#include "EventCounter.h"
+
+#include <boost/test/unit_test.hpp>
+
+static void resetGlobals()
+{
+}
+
+// ---------------------------------------------------------------------------
+
+BOOST_AUTO_TEST_SUITE(MultiThreadTestSuite)
+
+BOOST_AUTO_TEST_CASE (TestTwoLoopsWithManyEvents)
+{
+	// TODO: Implement me.
+	BOOST_CHECK(true);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
---
+++ code/tests/EventTest/SingleThreadTest.cpp
@@ -0,0 +1,160 @@
+/*    ___  _________     ____          __         
+     / _ )/ __/ ___/____/ __/___ ___ _/_/___ ___ 
+    / _  / _// (_ //___/ _/ / _ | _ `/ // _ | -_)
+   /____/_/  \___/    /___//_//_|_, /_//_//_|__/ 
+                               /___/             
+
+This file is part of the Brute-Force Game Engine, BFG-Engine
+
+For the latest info, see http://www.brute-force-games.com
+
+Copyright (c) 2012 Brute-Force Games GbR
+
+The BFG-Engine is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+The BFG-Engine is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with the BFG-Engine. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <boost/bind.hpp>
+#include <Base/EntryPoint.h>
+#include <EventSystem/Emitter.h>
+
+#include "EventCounter.h"
+
+#define BOOST_TEST_MODULE EventTest
+#include <boost/test/unit_test.hpp>
+
+size_t gEntryPointCalled = 0;
+
+static void resetGlobals()
+{
+	gEntryPointCalled = 0;
+}
+
+// ---------------------------------------------------------------------------
+
+static void* entryPoint(void* p)
+{
+	++gEntryPointCalled;
+	return 0;
+}
+
+// ---------------------------------------------------------------------------
+
+BOOST_AUTO_TEST_SUITE(SingleThreadTestSuite)
+
+BOOST_AUTO_TEST_CASE (TestLoopWithOneEvent)
+{
+	resetGlobals();
+	
+	EventLoop loop(false);
+	BFG::Emitter e(&loop);
+	TestEventCounter ter;
+	
+	loop.connect(TEST_EVENT_ID, &ter, &TestEventCounter::eventHandler);
+
+	// Shouldn't get delivered
+	e.emit<TestEvent>(TEST_EVENT_ID, TEST_EVENT_PAYLOAD);
+	BOOST_REQUIRE(ter.receivedEvents() == 0);
+
+	// Should get delivered
+	loop.doLoop();
+	BOOST_REQUIRE(ter.receivedEvents() == 1);
+
+	// Make sure it doesn't get delivered again
+	loop.doLoop();
+	BOOST_REQUIRE(ter.receivedEvents() == 1);
+}
+
+BOOST_AUTO_TEST_CASE (TestLoopWithOneEventAndDestinationAndSender)
+{
+	resetGlobals();
+	
+	const int OTHER_DESTINATION = 500;
+	const int DESTINATION = 999;
+	const int SENDER = 1000;
+
+	EventLoop loop(false);
+	BFG::Emitter e(&loop);
+	TestEventCounter ter;
+
+	loop.connect(TEST_EVENT_ID, &ter, &TestEventCounter::eventHandler, DESTINATION);
+
+	// Should not arrive since the DESTINATION is different
+	e.emit<TestEvent>(TEST_EVENT_ID, TEST_EVENT_PAYLOAD, OTHER_DESTINATION, SENDER);
+	loop.doLoop();
+	BOOST_REQUIRE(ter.receivedEvents() == 0);
+
+	// Should arrive since the DESTINATION is equal
+	e.emit<TestEvent>(TEST_EVENT_ID, TEST_EVENT_PAYLOAD, DESTINATION, SENDER);
+	loop.doLoop();
+	BOOST_REQUIRE(ter.receivedEvents() == 1);
+
+	// Should not arrive since it has no DESTINATION (which means: to all)
+	e.emit<TestEvent>(TEST_EVENT_ID, TEST_EVENT_PAYLOAD);
+	loop.doLoop();
+	BOOST_REQUIRE(ter.receivedEvents() == 1);
+}
+
+BOOST_AUTO_TEST_CASE (TestEntryPoint)
+{
+	resetGlobals();
+
+	EventLoop loop(false);
+
+	BFG::Base::CEntryPoint ep(&entryPoint);
+
+	// Shouldn't get entered yet
+	loop.addEntryPoint(&ep);
+	BOOST_REQUIRE(gEntryPointCalled == 0);
+
+	// Shouldn't get entered yet
+	loop.doLoop();
+	BOOST_REQUIRE(gEntryPointCalled == 0);
+
+	// Should get entered now
+	loop.stop();
+	loop.entry();
+	BOOST_REQUIRE(gEntryPointCalled == 1);
+
+	// Should get entered again
+	loop.stop();
+	loop.entry();
+	BOOST_REQUIRE(gEntryPointCalled == 2);
+}
+
+BOOST_AUTO_TEST_CASE (TestLoopEvent)
+{
+	resetGlobals();
+
+	EventLoop loop(true);
+	LoopEventCounter ler;
+	
+	// Shouldn't get delivered yet
+	loop.registerLoopEventListener(&ler, &LoopEventCounter::eventHandler);
+	BOOST_REQUIRE(ler.receivedEvents() == 0);
+
+	// Should get delivered now
+	loop.doLoop();
+	BOOST_REQUIRE(ler.receivedEvents() == 1);
+
+	// Make sure it does get delivered again
+	loop.doLoop();
+	BOOST_REQUIRE(ler.receivedEvents() == 2);
+
+	// Make sure it doesn't get delivered again
+	loop.unregisterLoopEventListener(&ler);
+	loop.doLoop();
+	BOOST_REQUIRE(ler.receivedEvents() == 2);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
