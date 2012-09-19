a0d1a0f850ebc34ac3c837227301f59731e56c6d
Sascha Wittkowski <wittus@gmx.de>
2012-09-19 19:36:52 +0200

    added more eventsystem tests

--- code/tests/EventTest/MultiThreadedTest.cpp
+++ code/tests/EventTest/MultiThreadedTest.cpp
@@ -25,6 +25,8 @@ along with the BFG-Engine. If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include <EventSystem/Emitter.h>
+#include <EventSystem/Core/CommunicationPolicy.h>
+#include <EventSystem/Core/ThreadingPolicy.h>
 
 #include "EventCounter.h"
 
@@ -34,14 +36,159 @@ static void resetGlobals()
 {
 }
 
+static void manyLoops(const std::string& name, IEventLoop* p)
+{
+	EventLoop* loop = static_cast<EventLoop*>(p);
+	for (size_t i=0; i<10000000; ++i)
+	{
+		long processedEvents = loop->doLoop();
+		if (processedEvents > 0)
+			std::cout << "Processed " << processedEvents << " events.\n";
+	}
+	std::cout << name << " done.\n";
+}
+
 // ---------------------------------------------------------------------------
 
 BOOST_AUTO_TEST_SUITE(MultiThreadTestSuite)
 
-BOOST_AUTO_TEST_CASE (TestTwoLoopsWithManyEvents)
+BOOST_AUTO_TEST_CASE (TestTwoLoopsWithoutCommunication)
 {
-	// TODO: Implement me.
-	BOOST_CHECK(true);
+	EventLoop loop1(false);
+	EventLoop loop2(false);
+	
+	BFG::Emitter e1(&loop1);
+	BFG::Emitter e2(&loop2);
+	
+	TestEventCounter ter;
+
+	e1.emit<TestEvent>(TEST_EVENT_ID, TEST_EVENT_PAYLOAD);
+
+	loop2.connect(TEST_EVENT_ID, &ter, &TestEventCounter::eventHandler);
+	
+	loop1.doLoop();
+	loop2.doLoop();
+
+	// Expect no received events since we arent't using "Interthread Communication"
+	BOOST_REQUIRE(ter.receivedEvents() == 0);
+}
+
+BOOST_AUTO_TEST_CASE (TestTwoLoopsWithOneWayCommunication)
+{
+	EventLoop loop1(false, new EventSystem::NoThread<>(), new EventSystem::InterThreadCommunication());
+	EventLoop loop2(false);
+	
+	BFG::Emitter e1(&loop1);
+	BFG::Emitter e2(&loop2);
+	
+	TestEventCounter ter;
+
+	e1.emit<TestEvent>(TEST_EVENT_ID, TEST_EVENT_PAYLOAD);
+
+	loop2.connect(TEST_EVENT_ID, &ter, &TestEventCounter::eventHandler);
+	
+	loop1.doLoop();
+	loop2.doLoop();
+
+	// Expect no received events since loop2 has no communication and does
+	// not poll if other pools are available.
+	BOOST_REQUIRE(ter.receivedEvents() == 0);
+}
+
+BOOST_AUTO_TEST_CASE (TestTwoLoopsWithCommunication)
+{
+	EventLoop loop1(false, new EventSystem::BoostThread<>("Loop1", &manyLoops), new EventSystem::InterThreadCommunication());
+	EventLoop loop2(false, new EventSystem::BoostThread<>("Loop2", &manyLoops), new EventSystem::InterThreadCommunication());
+	
+	BFG::Emitter e1(&loop1);
+	BFG::Emitter e2(&loop2);
+	
+	TestEventCounter ter;
+
+	// Sending 1000 events with Loop1 to an event handler of Loop2.
+	
+	loop2.connect(TEST_EVENT_ID, &ter, &TestEventCounter::eventHandler);
+
+	loop1.run();
+	loop2.run();
+
+	std::cout << "Sleep (100ms)" << std::endl;
+	boost::this_thread::sleep(boost::posix_time::microseconds(100));
+
+	const size_t EXPECTED_NUMBER_OF_EVENTS = 1000;
+	for (size_t i=0; i<EXPECTED_NUMBER_OF_EVENTS; ++i)
+	{
+		e1.emit<TestEvent>(TEST_EVENT_ID, TEST_EVENT_PAYLOAD);
+	}
+
+	std::cout << "Sleep (1)" << std::endl;
+	boost::this_thread::sleep(boost::posix_time::seconds(1));
+	std::cout << "Sleep (1)" << std::endl;
+	boost::this_thread::sleep(boost::posix_time::seconds(1));
+	std::cout << "Sleep (1)" << std::endl;
+	boost::this_thread::sleep(boost::posix_time::seconds(1));
+	std::cout << "Sleep (1)" << std::endl;
+	boost::this_thread::sleep(boost::posix_time::seconds(1));
+
+	loop1.setExitFlag();
+	loop2.setExitFlag();
+
+	std::cout << "Sleep (1)" << std::endl;
+	boost::this_thread::sleep(boost::posix_time::seconds(1));
+	std::cout << "Sleep (1)" << std::endl;
+	boost::this_thread::sleep(boost::posix_time::seconds(1));
+	
+	std::cout << "Received Events: " << ter.receivedEvents() << std::endl;
+	
+	// Expect EXPECTED_NUMBER_OF_EVENTS received events. Both loops should communicate.
+	BOOST_CHECK_EQUAL(ter.receivedEvents(), EXPECTED_NUMBER_OF_EVENTS);
+}
+
+BOOST_AUTO_TEST_CASE (TestCreatePoolElementException)
+{
+	EventLoop loop1(false, new EventSystem::BoostThread<>("Loop1", &manyLoops), new EventSystem::InterThreadCommunication());
+	
+	BFG::Emitter e1(&loop1);
+	
+	TestEventCounter ter;
+
+	// Provoking "unable to create pool element"
+	
+	loop1.connect(TEST_EVENT_ID, &ter, &TestEventCounter::eventHandler);
+	loop1.run();
+
+	std::cout << "Sleep (100ms)" << std::endl;
+	boost::this_thread::sleep(boost::posix_time::microseconds(100));
+
+	const size_t EXPECTED_NUMBER_OF_EVENTS = 1000000;
+
+	BOOST_CHECK_NO_THROW(
+		for (size_t i=0; i<EXPECTED_NUMBER_OF_EVENTS; ++i)
+		{
+			e1.emit<TestEvent>(TEST_EVENT_ID, TEST_EVENT_PAYLOAD);
+		}
+	);
+
+	std::cout << "Sleep (1)" << std::endl;
+	boost::this_thread::sleep(boost::posix_time::seconds(1));
+	std::cout << "Sleep (1)" << std::endl;
+	boost::this_thread::sleep(boost::posix_time::seconds(1));
+	std::cout << "Sleep (1)" << std::endl;
+	boost::this_thread::sleep(boost::posix_time::seconds(1));
+	std::cout << "Sleep (1)" << std::endl;
+	boost::this_thread::sleep(boost::posix_time::seconds(1));
+
+	loop1.setExitFlag();
+
+	std::cout << "Sleep (1)" << std::endl;
+	boost::this_thread::sleep(boost::posix_time::seconds(1));
+	std::cout << "Sleep (1)" << std::endl;
+	boost::this_thread::sleep(boost::posix_time::seconds(1));
+	
+	std::cout << "Received Events: " << ter.receivedEvents() << std::endl;
+	
+	// Expect EXPECTED_NUMBER_OF_EVENTS received events without error.
+	BOOST_CHECK_EQUAL(ter.receivedEvents(), EXPECTED_NUMBER_OF_EVENTS);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
