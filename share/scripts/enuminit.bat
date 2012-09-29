9b4a56441dd0d879a4a32924f19295d5b1f5381e
Sascha Wittkowski <wittus@gmx.de>
2012-09-29 15:05:42 +0200

    added missing Network entry for enuminit.bat

--- share/scripts/enuminit.bat
+++ share/scripts/enuminit.bat
@@ -3,6 +3,7 @@ type NUL > ./code/engine/Audio/Enums.hh
 type NUL > ./code/engine/Controller/Enums.hh
 type NUL > ./code/engine/Core/Enums.hh
 type NUL > ./code/engine/Model/Enums.hh
+type NUL > ./code/engine/Network/Enums.hh
 type NUL > ./code/engine/Physics/Enums.hh
 type NUL > ./code/engine/View/Enums.hh
 
@@ -10,5 +11,6 @@ type NUL > ./code/engine/Audio/Enums.cpp
 type NUL > ./code/engine/Controller/Enums.cpp
 type NUL > ./code/engine/Core/Enums.cpp
 type NUL > ./code/engine/Model/Enums.cpp
+type NUL > ./code/engine/Network/Enums.cpp
 type NUL > ./code/engine/Physics/Enums.cpp
 type NUL > ./code/engine/View/Enums.cpp
