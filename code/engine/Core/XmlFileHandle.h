4e875fe13df61716b3fdc69ce1dda72728411892
Sascha <sascha_axt@web.de>
2012-10-20 14:40:07 +0200

    New interface for xml file operations.

---
+++ code/engine/Core/XmlFileHandle.h
@@ -0,0 +1,54 @@
+/*    ___  _________     ____          __         
+     / _ )/ __/ ___/____/ __/___ ___ _/_/___ ___ 
+    / _  / _// (_ //___/ _/ / _ | _ `/ // _ | -_)
+   /____/_/  \___/    /___//_//_|_, /_//_//_|__/ 
+                               /___/             
+
+This file is part of the Brute-Force Game Engine, BFG-Engine
+
+For the latest info, see http://www.brute-force-games.com
+
+Copyright (c) 2012 Brute-Force Games GbR
+
+The BFG-Engine is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+The BFG-Engine is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with the BFG-Engine. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef BFG_XML_FILE_HANDLE__
+#define BFG_XML_FILE_HANDLE__
+
+#include <boost/shared_ptr.hpp>
+#include <core/XmlTree.h>
+
+
+namespace BFG {
+
+class XmlFileHandle
+{
+
+public:
+
+	XmlFileHandle(const std::string& path): mPath(path) {}
+
+	virtual void save(const std::string& = "") = 0;
+	boost::shared_ptr<XmlTree> root() { return mRoot; }
+
+protected:
+
+	std::string mPath;
+	boost::shared_ptr<XmlTree> mRoot;
+};
+
+} // namespace BFG
+
+#endif
---
+++ code/engine/Core/XmlTree.h
@@ -0,0 +1,53 @@
+/*    ___  _________     ____          __         
+     / _ )/ __/ ___/____/ __/___ ___ _/_/___ ___ 
+    / _  / _// (_ //___/ _/ / _ | _ `/ // _ | -_)
+   /____/_/  \___/    /___//_//_|_, /_//_//_|__/ 
+                               /___/             
+
+This file is part of the Brute-Force Game Engine, BFG-Engine
+
+For the latest info, see http://www.brute-force-games.com
+
+Copyright (c) 2012 Brute-Force Games GbR
+
+The BFG-Engine is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+The BFG-Engine is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with the BFG-Engine. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef BFG_XML_TREE__
+#define BFG_XML_TREE__
+
+#include <boost/shared_ptr.hpp>
+
+namespace BFG {
+
+class XmlTree
+{
+
+public:
+
+	XmlTree(boost::shared_ptr<XmlTree> copyObject);
+
+	virtual boost::shared_ptr<XmlTree> child(const std::string& name) = 0;
+	virtual std::vector<XmlTree> childList(const std::string& name) = 0;
+	virtual std::string attribute(const std::string& name) = 0;
+
+	virtual void addElement(const std::string& name, const std::string& value) = 0;
+	virtual void addAttribute(const std::string& name, const std::string& value) = 0;
+	virtual void addElement(const std::string& name, boost::shared_ptr<XmlTree> value) = 0;
+
+};
+
+} // namespace BFG
+
+#endif
