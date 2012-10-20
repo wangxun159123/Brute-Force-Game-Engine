b5768fcad4d23c40ca707c508f8d42e96e347d0e
Sascha <sascha_axt@web.de>
2012-10-20 18:52:42 +0200

    Added PugiXmlTree and fixed some bugs.

---
+++ code/engine/Core/PugiXmlTree.cpp
@@ -0,0 +1,74 @@
+/*    ___  _________     ____          __         
+     / _ )/ __/ ___/____/ __/___ ___ _/_/___ ___ 
+    / _  / _// (_ //___/ _/ / _ | _ `/ // _ | -_)
+   /____/_/  \___/    /___//_//_|_, /_//_//_|__/ 
+                               /___/             
+
+This file is part of the Brute-Force Game Engine, BFG-Engine
+
+For the latest info, see http://www.brute-force-games.com
+
+Copyright (c) 2012 Brute-Force Games GbR
+
+The BFG-Engine is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+The BFG-Engine is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with the BFG-Engine. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#include <core/PugiXmlTree.h>
+
+namespace BFG {
+
+	boost::shared_ptr<XmlTree> PugiXmlTree::child(const std::string& name)
+	{
+		pugi::xml_node node = mXmlNode.child(name.c_str());
+		return boost::shared_ptr<XmlTree>(new PugiXmlTree(node));
+	}
+
+	std::vector<boost::shared_ptr<XmlTree> > PugiXmlTree::childList(const std::string& name)
+	{
+		std::vector<boost::shared_ptr<XmlTree> > treeList;
+
+		for (pugi::xml_node node = mXmlNode.child(name.c_str()); node; node = node.next_sibling(name.c_str()))
+		{
+			treeList.push_back(boost::shared_ptr<XmlTree>(new PugiXmlTree(node)));
+		}
+
+		return treeList;
+	}
+
+	std::string PugiXmlTree::attribute(const std::string& name)
+	{
+		return mXmlNode.attribute(name.c_str()).value();
+	}
+
+	boost::shared_ptr<XmlTree> PugiXmlTree::addElement(const std::string& name, const std::string& value)
+	{
+		pugi::xml_node node = mXmlNode.append_child();
+		node.set_name(name.c_str());
+		node.set_value(value.c_str());
+
+		return boost::shared_ptr<XmlTree>(new PugiXmlTree(node));
+	}
+
+	void PugiXmlTree::addAttribute(const std::string& name, const std::string& value)
+	{
+		mXmlNode.append_attribute(name.c_str()).set_value(value.c_str());
+	}
+
+	void PugiXmlTree::addElement(boost::shared_ptr<XmlTree> value)
+	{
+		//tbd
+	}
+
+} // namespace BFG
--- code/engine/Core/PugiXmlTree.h
+++ code/engine/Core/PugiXmlTree.h
@@ -1,4 +1,4 @@
-/*    ___  _________     ____          __         
+/*    ___  _________     ____          __
      / _ )/ __/ ___/____/ __/___ ___ _/_/___ ___ 
     / _  / _// (_ //___/ _/ / _ | _ `/ // _ | -_)
    /____/_/  \___/    /___//_//_|_, /_//_//_|__/ 
@@ -24,26 +24,36 @@ You should have received a copy of the GNU Lesser General Public License
 along with the BFG-Engine. If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef BFG_XML_TREE__
-#define BFG_XML_TREE__
+#ifndef BFG_PUGI_XML_TREE__
+#define BFG_PUGI_XML_TREE__
 
-#include <vector>
-#include <boost/shared_ptr.hpp>
+#include <core/XmlTree.h>
+#include <pugixml.hpp>
 
 namespace BFG {
 
-class XmlTree
+class PugiXmlTree : public XmlTree
 {
 
 public:
 
-	virtual boost::shared_ptr<XmlTree> child(const std::string& name) = 0;
-	virtual std::vector<boost::shared_ptr<XmlTree> > childList(const std::string& name) = 0;
-	virtual std::string attribute(const std::string& name) = 0;
+	boost::shared_ptr<XmlTree> child(const std::string& name);
+	std::vector<boost::shared_ptr<XmlTree> > childList(const std::string& name);
+	std::string attribute(const std::string& name);
 
-	virtual boost::shared_ptr<XmlTree> addElement(const std::string& name, const std::string& value) = 0;
-	virtual void addAttribute(const std::string& name, const std::string& value) = 0;
-	virtual void addElement(boost::shared_ptr<XmlTree> value) = 0;
+	boost::shared_ptr<XmlTree> addElement(const std::string& name, const std::string& value);
+	void addAttribute(const std::string& name, const std::string& value);
+	void addElement(boost::shared_ptr<XmlTree> value);
+
+private:
+
+	PugiXmlTree(pugi::xml_node& node): mXmlNode(node) {}
+	PugiXmlTree(const PugiXmlTree& copy)
+	{
+		mXmlNode = copy.mXmlNode;
+	}
+
+	pugi::xml_node mXmlNode;
 };
 
 } // namespace BFG
