30cbf005ef2efcb275e1155524218301f1eb027f
Mephys <sascha_axt@web.de>
2012-11-03 14:26:06 +0100

    Added test for childList.

--- code/tests/LoaderTest/main.cpp
+++ code/tests/LoaderTest/main.cpp
@@ -30,55 +30,25 @@ BOOST_AUTO_TEST_CASE (loadFile)
 }
 
 
-BOOST_AUTO_TEST_CASE (loadFile)
+BOOST_AUTO_TEST_CASE (elementList)
 {
-	Base::Logger::Init(Base::Logger::SL_DEBUG, "Logs/LoaderTest.log");
+    //Base::Logger::Init(Base::Logger::SL_DEBUG, "Logs/LoaderTest.log");
 
-	Path path;
-	std::string xml_test_file_path = "";
-	xml_test_file_path = path.Expand("LoaderTest.xml");
+    Path path;
+    std::string xml_test_file_path = "";
+    xml_test_file_path = path.Expand("LoaderTest.xml");
 
-	boost::shared_ptr<XmlFileHandle> fileHandle;
-	fileHandle.reset(new PugiXmlFileHandle(xml_test_file_path));
+    boost::shared_ptr<XmlFileHandle> fileHandle;
+    fileHandle.reset(new PugiXmlFileHandle(xml_test_file_path));
 
-	
+    XmlTreeT searchHandle;
+    BOOST_REQUIRE_NO_THROW(searchHandle = fileHandle->root()->child("root"));
+    XmlTreeListT elements;
 
+    BOOST_REQUIRE_NO_THROW(elements = searchHandle->childList("element"));
+
+    BOOST_REQUIRE_EQUAL(elements.size(), 3);
 }
-
-
-
-
-
-
-//BOOST_AUTO_TEST_CASE (distance)
-//{
-//	BFG::v3 a(-5.2, 3.8, 4.8);
-//	BFG::v3 b(8.7, -4.1, 9.1);
-
-//	BFG::f32 result = BFG::distance(a,b);
-//	BFG::f32 expected = 16.5563f;
-
-//	const BFG::f32 ERROR_MARGIN = 0.0001f;
-
-//	bool resultCorrect = std::fabs(result - expected) < ERROR_MARGIN;
-
-//	BOOST_REQUIRE (resultCorrect);
-//}
-
-//BOOST_AUTO_TEST_CASE (nearEnough)
-//{
-//	// distance between `a' and `b' is 16.5563f
-//	BFG::v3 a(-5.2, 3.8, 4.8);
-//	BFG::v3 b(8.7, -4.1, 9.1);
-
-//	bool resultNull = BFG::nearEnough(a, b, 0.0);
-//	bool resultLower = BFG::nearEnough(a, b, 16.5);
-//	bool resultUpper = BFG::nearEnough(a, b, 16.6);
-
-//	BOOST_REQUIRE (resultNull == false);
-//	BOOST_REQUIRE (resultUpper == true);
-//	BOOST_REQUIRE (resultLower == false);
-//}
 
 }
 }
