271795d884613653c314a270fabfe594b7ee675b
Sascha Wittkowski <wittus@gmx.de>
2012-11-17 16:13:24 +0100

    linux fix

--- code/engine/Core/XmlTree.h
+++ code/engine/Core/XmlTree.h
@@ -29,7 +29,7 @@ along with the BFG-Engine. If not, see <http://www.gnu.org/licenses/>.
 
 #include <vector>
 #include <boost/shared_ptr.hpp>
-#include <core/Defs.h>
+#include <Core/Defs.h>
 
 namespace BFG {
 
