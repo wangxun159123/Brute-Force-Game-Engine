1f59b22cdd3e88e47b286cf75a84746ab1f6f2e9
Mephys <sascha_axt@web.de>
2013-03-17 15:03:03 +0100

    Fixed broken compiler support for vs2010.

--- share/cmake/Modules/FindAllDependencies.cmake
+++ share/cmake/Modules/FindAllDependencies.cmake
@@ -8,7 +8,7 @@ MACRO(FIND_ALL_DEPENDENCIES)
 
 #	set(Boost_DEBUG ON)
 
-	FIND_PACKAGE(Boost 1.49 REQUIRED date_time filesystem program_options regex serialization system thread unit_test_framework)
+	FIND_PACKAGE(Boost 1.${BOOST_MINOR_VERSION_NR} REQUIRED date_time filesystem program_options regex serialization system thread unit_test_framework)
 
 	IF(Boost_FOUND)
 		LOG("Found boost ${Boost_VERSION} in ${Boost_LIBRARY_DIRS} and ${Boost_INCLUDE_DIR}")
